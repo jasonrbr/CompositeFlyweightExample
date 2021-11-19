7fef7ef4aaa287ed4b225ef8603e2f8969b7541f
Jason Brown <jasonrbr17@gmail.com>
2021-11-19 06:14:21 -0500

    Add title and cleanup code

--- flyweight.groovy
+++ flyweight.groovy
@@ -4,55 +4,47 @@ class EmployeeContext {
 }
 
 class Employee {
-
-    Employee(long aSalary) {
-        this.salary = aSalary
-    }
-
     long salary
-
-    def long getSalary() {
-        salary
-    }
+    String title
 
     def getDescription(EmployeeContext c) {
-        println("${c.name} makes ${this.getSalary()} working for ${c.project}")
+        println("${c.name} is a ${this.title} who makes ${this.salary} working for ${c.project}")
     }
 }
 
 class EmployeeFactory {
     def static employeeMap = [:]
 
-    def static getEmployee(String type) {
-        if (this.employeeMap.containsKey(type)) {
-            return this.employeeMap[type]
+    def static CreateEmployee(String title) {
+        if (this.employeeMap.containsKey(title)) {
+            return this.employeeMap[title]
         }
 
-        switch(type) {
+        switch(title) {
             case 'Consultant':
-                this.employeeMap[type] = new Employee(203000)
+                this.employeeMap[title] = new Employee(title: title, salary: 203000)
                 break
             case 'SeniorConsultant':
-                this.employeeMap[type] = new Employee(220000)
+                this.employeeMap[title] = new Employee(title: title, salary: 220000)
                 break
             case 'AssociateDirector':
-                this.employeeMap[type] = new Employee(250000)
+                this.employeeMap[title] = new Employee(title: title, salary: 250000)
                 break
             case 'Director':
-                this.employeeMap[type] = new Employee(300000)
+                this.employeeMap[title] = new Employee(title: title, salary: 300000)
                 break
             case 'Principal':
-                this.employeeMap[type] = new Employee(1000000)
+                this.employeeMap[title] = new Employee(title: title, salary: 1000000)
                 break
             default:
-                throw new RuntimeException("No matching class for type: ${type}")
+                throw new RuntimeException("No matching class for title: ${title}")
         }
 
-        this.employeeMap[type]
+        this.employeeMap[title]
     }
 }
 
-def director = EmployeeFactory.getEmployee('Director')
+def director = EmployeeFactory.CreateEmployee('Director')
 def dariusContext = new EmployeeContext(name: 'Darius', project: 'Cool Modernizing Insurance')
 def mattContext = new EmployeeContext(name: 'Matt', project: 'Very Awesome Data Modernizing')
 def priContext = new EmployeeContext(name: 'Pri', project: 'Silly Canadian Insurance')
